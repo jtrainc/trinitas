573372e425ebb109e14984e1492ef458a9b719ad
jtrainc <jackson.cowart@trinitaschristian.org>
2025-04-15 19:07:48 +0000

    work very hard on our new file, scrips.js is coming along.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,42 @@
+//These are all "const" bcause we expect no changes to be made to the value
+const h1 = document.getElementById("title");
+const h2 = document.querySelector("h2");
 const el = document.querySelector("#currenttime");
+const username = prompt("What is your name, traveler?");
 
-setInterval(setTime, 1000);
+//Types of values
+let string_type = "Hello";
+
+
+
+
+//We are going to change this value at least once more
+let timevalue;
+
+// Can the use see our secret content?
+let tries = 10;
+let userAnswer = prompt("What is the secret message?");
+
+while (userAnswer != "please" && tries > 0) {
+    alert("Wrong. Try again. You have " + tries + " attempts remaining.");
+    userAnswer = prompt("What is the secret message?");
+    tries = tries - 1;
+}
+if (userAnswer == "please") {
+    h2.hidden = false;
+}
+
+// Update our clock once every second
+setInterval(setTime, 1_000);
+
+// Greet the user by the name they provided
+h1.textContent = "Hello, " + username;
 
 function setTime() {
-    el.textContent = new Date().toLocaleTimeString();
+    timevalue = new Date().toLocaleTimeString();
+    renderTime();
+}
+
+function renderTime() {
+    el.textContent = timevalue;
 }
