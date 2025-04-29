fa5378e1457de263ca5331110821881da5a238f4
jtrainc <jackson.cowart@trinitaschristian.org>
2025-04-29 19:07:15 +0000

    idkwibt, its all broken but we are going to save this anyway

--- oo/index.js
+++ oo/index.js
@@ -1,35 +1,57 @@
 //Machine class is our base class is our base class; it doesn't extend anything. It provides the basis of all other machine_type objects and classes.
 class Machine {
     weight;
-    coords = [0, 0];
-    constructor(in_weight = 100, x_coord = 10, y_coord = 10) {
+    location;
+    color;
+    element;
+    angle;
+    constructor(in_weight = 100, x_coord = 10, y_coord = 10, color = "gray") {
         console.log("Creating machine with wieght of " + in_weight + " pounds");
         console.log("Oh, and it has coords of " + [x_coord, y_coord]);
         this.weight = in_weight;
-        this.coords = [x_coord, y_coord];
+        this.location = [x_coord, y_coord];
+        this.color = color;
+        this.element = null;
+        this.angle = angle;
+    }
+
+    render() {
+        if (this.element) {
+            return;
+        }
+
+        this.element = document.createElement("div");
+        this.element.style.backgroundColor = this.color;
+        this.element.style.width = "30px";
+        this.element.style.height = "10px";
+        this.element.style.transforms = "rotate(" + this.angle + "rad)";
+
+
+        document.body.appendChild(this.element);
     }
 }
-
 class Automobile extends Machine {
-    constructor(in_fuel, in_weight = 2000) {
-        super(in_weight);
+    fuel;
+    moveSpeed;
+    constructor(in_fuel = 1, in_weight = 2000, color = "gray", moveSpeed = 10) {
+        super(in_weight, 10, 10, color);
+        this.moveSpeed = moveSpeed;
         console.log("you've created an automobile with a tank that is " + in_fuel + "% full");
         this.fuel = in_fuel;
     }
 }
 
 class Tank extends Automobile {
-    weight;
-    fuel;
-    color = "gray";
-    constructor (color) {
-        super( 1, 10_000);
-        this.color = color;
+    rounds;
+    constructor(rounds = 1_000, color = "green") {
+        super(1, 10_000, color);
+        this.rounds = rounds;
         console.log("Your tank is alive?");
     }
 }
 
-const armyTank = new Tank("green");
-const vehicle = new Automobile(0.5);
+const armyTank = new Tank(1000, "green");
 
-const printer = new Machine(500, 32, 55);
+armyTank.render();
+//const vehicle = new Automobile(0.5);
+//const printer = new Machine(500, 32, 55);
